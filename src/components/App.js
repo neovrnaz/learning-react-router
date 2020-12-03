742471f42387d54c2cfb2b7589037901e77305d4
neovrnaz <neovrnaz@gmail.com>
2020-12-03 00:38:41 -0700

    Add constructor

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 const App = () => (
-  <div className="container">
-
-  </div>
+  <BrowserRouter>
+    <div className="container">
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About title="About" />
+      </Route>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
-class Home extends Component {    
-  
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="main-content home">
@@ -10,7 +14,6 @@ class Home extends Component {
         <p>Learn front end web development and much more! This simple directory app offers a preview of our course library. Choose from many hours of content, from HTML to CSS to JavaScript. Learn to code and get the skills you need to launch a new career in front end web development.</p>
         <p>We have thousands of videos created by expert teachers on web design and front end development. Our library is continually refreshed with the latest on web technology so you will never fall behind.</p>
         <hr />
-        
       </div>
     );
   }
